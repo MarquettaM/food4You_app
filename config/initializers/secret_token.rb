44ecda1be5dee30bded4e7f9419b3a01c8d63eec
' <'>
2014-01-23 14:45:35 -0600

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Food4YouApp::Application.config.secret_key_base = 'df73e6c3ab012e64aeb857a5f155ca0ff706f536545fc0dc72f63a13c71420d9fdea923927db739618e448d215d39efb3d0528b6689b3a23c7633bf232cf02f1'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Food4YouApp::Application.config.session_store :cookie_store, key: '_food4You_app_session'
